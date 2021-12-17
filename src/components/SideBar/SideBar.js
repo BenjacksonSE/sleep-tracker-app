ca14fb383fd86b2e4ac08a8a61072e1cdc864120
Ben Jackson <benjacksonSE@gmail.com>
2021-12-17 09:15:14 -0500

    Init commit

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,12 @@
+import classes from './SideBar.module.scss'
+
+const SideBar = () => {
+  return (
+    <div className={classes.sidebar__container}>
+      <div className={classes.sidebar}>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import SideBar from "../components/SideBar/SideBar"
+import Dashboard from "../components/Dashboard/Dashboard"
+
+const Home = () => {
+  return (
+    <>
+      <SideBar />
+      {/* <Dashboard /> */}
+    </>
+  )
+}
+
+export default Home
